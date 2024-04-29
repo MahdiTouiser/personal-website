d34a4b3039d695c1354175578ae28bc6e3fb7b38
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-04-29 11:45:05 +0330

    Fix the styling codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import { Box } from '@mui/material';
+import React, { useRef } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import Introduction from './components/Introduction';
 import Navbar from './components/Navbar';
 
-const App = () => {
+const App: React.FC = () => {
+  const introductionRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const scrollToRef = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <Router>
-      <Box className='app-container'>
-        <Navbar />
-        <section id="introduction" className="section">
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+        <Navbar
+          scrollToRef={{
+            introductionRef,
+            aboutRef,
+          }}
+        />
+        <section id="introduction" ref={introductionRef}>
           <Introduction />
         </section>
-        <section id="about" className="section">
+        <section id="about" ref={aboutRef}>
           <About />
         </section>
       </Box>
--- src/components/IntroductionElements/IntroductionImage.tsx
+++ src/components/IntroductionElements/IntroductionImage.tsx
@@ -1,9 +1,9 @@
-import { Box } from "@mui/material"
+import { Avatar, Box } from "@mui/material"
 
 const IntroductionImage = () => {
     return (
-        <Box className="image-container">
-            <img src={"public/hero-image.jpg"} alt="hero image" className="image" />
+        <Box sx={{ position: 'relative', width: '300px', height: '300px' }}>
+            <Avatar src={"public/hero-image.jpg"} alt="hero image" sx={{ width: '100%', height: '100%', objectFit: 'cover', border: '5px solid white', borderRadius: '50%' }} />
         </Box>
     )
 }
